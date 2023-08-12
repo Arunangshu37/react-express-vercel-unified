b8fe83c72975687a1626f8e496729582bf45e310
Arunangshu Biswas <arunangshu.biswas.x@gmail.com>
2023-08-12 12:27:24 +0100

    correction in routes

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ var app = express();
 app.use(express.static('vision/build'));
 app.use(cors())
 app.use('/api', router);
-app.get("*", (req, res) => {
+
+app.use(express.static(path.join(__dirname , "/vision/build"))) 
+
+app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "vision", "build", "index.html"))
 });
 
